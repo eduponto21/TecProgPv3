8161f751bbe40b208cd38bdcf7a4ca156c2e7c15
Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
2017-06-07 11:58:32 -0300

    Versão inicial.

---
+++ src/main/java/Pratica.java
@@ -0,0 +1,13 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * 
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ */
+public class Pratica {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ src/main/java/utfpr/ct/dainf/pratica/Poligonal.java
@@ -0,0 +1,16 @@
+package utfpr.ct.dainf.pratica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * 
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ * @param <T> Tipo do ponto
+ */
+public class Poligonal<T extends Ponto2D> {
+    private final List<T> vertices = new ArrayList<>();
+
+}
